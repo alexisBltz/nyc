84e3d93c7a0c7303d80c651179d3dd303efcf82d
alexisBltz <almamanima@unsa.edu.pe>
2025-03-01 01:48:09 -0500

    Añadiendo react a las islas de astro, instalando iconos e lucide

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import sanity from "@sanity/astro";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [
-        sanity({
-            projectId: "504b2zku",
-            dataset: "production",
-            useCdn: false, // for static builds
-        }),
-    ],
+    integrations: [sanity({
+        projectId: "504b2zku",
+        dataset: "production",
+        useCdn: false, // for static builds
+    }), react()],
 });
